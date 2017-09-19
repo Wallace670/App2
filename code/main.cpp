f9a635fb5480fe49e9fa42c0ee6f8868f337f5da
Paul SAINRAT <pl.sainrat@outlook.com>
2017-09-19 17:22:52 -0400

    ajout des commentaires

--- code/main.cpp
+++ code/main.cpp
@@ -50,121 +50,124 @@ void date(event* addMemPool)
 {
   time_t seconds = time(NULL);
   strftime(addMemPool->date, 30, "%y:%m:%d:%I:%M:%S ", localtime(&seconds));
- }
-
-  /*----------------------------------------------------------------------------
-    FONCTION ATTACHEES AU THREAD
-    ---------------------------------------------------------------------------*/
-
-
-  void lecture_analog()
-  {
-    int count = 0;
-    int inputValues[2];
-    int moyennes_ea1[2] = {0.0, 0.0};
-    int moyennes_ea2[2] = {0.0, 0.0};
-    while (true) {
-      if(count == 4) {
-    inputValues[0] = 0;
-    inputValues[1] = 0;
-
-    for(int i=0; i<5; i++) {
-      inputValues[0] += ea_1.read()*1000;
-      inputValues[1] += ea_2.read()*1000;
-      Thread::signal_wait(0x01);
-    }
-
-    moyennes_ea1[0] = inputValues[0]/5;
-    moyennes_ea2[0] = inputValues[1]/5;
+}
+ 
+/*----------------------------------------------------------------------------
+  FONCTION ATTACHEES AU THREAD
+  ---------------------------------------------------------------------------*/
 
 
-    if(abs(moyennes_ea1[0] - moyennes_ea1[1]) > 125) {
-      event *alog1 = deadPool.alloc();
-      alog1->TYPE=ANA;
-      alog1->val=(moyennes_ea1[0]*3.3)/1000;
-      alog1->pin=19;
-      date(alog1);
-      queueEvent.put(alog1);
-    }
+void lecture_analog()
+{
+  int count = 0;
+  int inputValues[2];
+  int moyennes_ea1[2] = {0.0, 0.0};
+  int moyennes_ea2[2] = {0.0, 0.0};
+  while (true) {
+    if(count == 4) {
+      inputValues[0] = 0;
+      inputValues[1] = 0;
 
-    if(abs(moyennes_ea2[0] - moyennes_ea2[1]) > 125) {
-      event* alog2 = deadPool.alloc();
-      alog2->TYPE=ANA;
-      alog2->val=(moyennes_ea2[0]*3.3)/1000;
-      alog2->pin=20;
-      date(alog2);
-      queueEvent.put(alog2);
-    }
-
-    moyennes_ea1[1] = moyennes_ea1[0];
-    moyennes_ea2[1] = moyennes_ea2[0];
-    count = 0;
+      for(int i=0; i<5; i++) {
+	inputValues[0] += ea_1.read()*1000;
+	inputValues[1] += ea_2.read()*1000;
+	Thread::signal_wait(0x01);
       }
-      else{
-    count++;
+
+      moyennes_ea1[0] = inputValues[0]/5;
+      moyennes_ea2[0] = inputValues[1]/5;
+
+
+      if(abs(moyennes_ea1[0] - moyennes_ea1[1]) > 125) {
+	//allocation d'une case dans la memoryPool
+	event *alog1 = deadPool.alloc();
+	//on remplis la structure avec les données
+	alog1->TYPE=ANA;
+	alog1->val=(moyennes_ea1[0]*3.3)/1000;
+	alog1->pin=19;
+	date(alog1);
+	queueEvent.put(alog1);
       }
-      Thread::signal_wait(0x01);
+
+      if(abs(moyennes_ea2[0] - moyennes_ea2[1]) > 125) {
+	//allocation d'une case dans la memoryPool
+	event* alog2 = deadPool.alloc();
+	//on remplis la structure avec les données
+	alog2->TYPE=ANA;
+	alog2->val=(moyennes_ea2[0]*3.3)/1000;
+	alog2->pin=20;
+	date(alog2);
+	queueEvent.put(alog2);
+      }
+
+      moyennes_ea1[1] = moyennes_ea1[0];
+      moyennes_ea2[1] = moyennes_ea2[0];
+      count = 0;
     }
-  }
-
-
-
-
-  void lecture_num1()
-  {
-    bool numVal = 0;
-    bool numVal_old = numVal;
-    bool numFlag = false;
-    while (true) {
-      if(numFlag == true) {
-    numVal = en_1;
-    if(numVal_old != numVal) {
-      Thread::signal_wait(0x1);
-      numVal = en_1;
-      if(numVal_old != numVal) {
-        event* num1 = deadPool.alloc();
-        num1->TYPE=NUM;
-        num1->val=numVal-numVal_old;
-        num1->pin=15;
-        date(num1);
-        numVal_old = numVal;
-        queueEvent.put(num1);
-      }
-    }
-      }
-      numFlag = !numFlag;
-      Thread::signal_wait(0x1);
-    }
-  }
-
-
-  void lecture_num2()
-  {
-    bool numVal = 0;
-    bool numVal_old = numVal;
-    bool numFlag = false;
-    while (true) {
-      if(numFlag == true) {
-    numVal = en_2;
-    if(numVal_old != numVal) {
-      Thread::signal_wait(0x1);
-      numVal = en_2;
-      if(numVal_old != numVal) {
-        event* num2 = deadPool.alloc();
-        num2->TYPE=NUM;
-        num2->val=numVal-numVal_old;
-        num2->pin=16;
-        date(num2);
-        numVal_old = numVal;
-        queueEvent.put(num2);
-      }
-    }
-      }
-      numFlag = !numFlag;
-      Thread::signal_wait(0x1);
+    else{
+      count++;
     }
     Thread::signal_wait(0x01);
   }
+}
+
+
+
+
+void lecture_num1()
+{
+  bool numVal = 0;
+  bool numVal_old = numVal;
+  bool numFlag = false;
+  while (true) {
+    if(numFlag == true) {
+      numVal = en_1;
+      if(numVal_old != numVal) {
+	Thread::signal_wait(0x1);
+	numVal = en_1;
+	if(numVal_old != numVal) {
+	  event* num1 = deadPool.alloc();
+	  num1->TYPE=NUM;
+	  num1->val=numVal-numVal_old;
+	  num1->pin=15;
+	  date(num1);
+	  numVal_old = numVal;
+	  queueEvent.put(num1);
+	}
+      }
+    }
+    numFlag = !numFlag;
+    Thread::signal_wait(0x1);
+  }
+}
+
+
+void lecture_num2()
+{
+  bool numVal = 0;
+  bool numVal_old = numVal;
+  bool numFlag = false;
+  while (true) {
+    if(numFlag == true) {
+      numVal = en_2;
+      if(numVal_old != numVal) {
+	Thread::signal_wait(0x1);
+	numVal = en_2;
+	if(numVal_old != numVal) {
+	  event* num2 = deadPool.alloc();
+	  num2->TYPE=NUM;
+	  num2->val=numVal-numVal_old;
+	  num2->pin=16;
+	  date(num2);
+	  numVal_old = numVal;
+	  queueEvent.put(num2);
+	}
+      }
+    }
+    numFlag = !numFlag;
+    Thread::signal_wait(0x1);
+  }
+}
 
 
 
@@ -172,24 +175,29 @@ void date(event* addMemPool)
 void collection()
 {
   while (true) {
+    //récupération de l'adresse de l'eveneent dans la queue
     osEvent evt = queueEvent.get();
-        if(evt.status == osEventMessage) {
-            event *addEvent= (event*)evt.value.p;
-            if(addEvent->TYPE==ANA) {
-                printf("%s , type : analogique , valeur = %lf , pin : %d \n",addEvent->date,addEvent->val,addEvent->pin);
-                deadPool.free(addEvent);
-            } else {
-                if(addEvent->val<0){
-                printf("%s , type : numérique , Front montant , pin : %d \n",addEvent->date,addEvent->pin);
-                }
-                else{    
-                printf("%s , type : numérique , Front descendant , pin : %d \n",addEvent->date,addEvent->pin);
-                }
-                deadPool.free(addEvent);    
-            }
-        }
+    if(evt.status == osEventMessage) {
+      event *addEvent= (event*)evt.value.p;
+      //on fait des opération différentes selon la nature de l'evenement
+      if(addEvent->TYPE==ANA) {
+	printf("%s , type : analogique , valeur = %lf , pin : %d \n",addEvent->date,addEvent->val,addEvent->pin);
+	deadPool.free(addEvent);
+      } else {
+	if(addEvent->val<0){
+	  printf("%s , type : numérique , Front montant , pin : %d \n",addEvent->date,addEvent->pin);
+	}
+	else{    
+	  printf("%s , type : numérique , Front descendant , pin : %d \n",addEvent->date,addEvent->pin);
+	}
+	
+      }
+      //on libère la case dans la memoryPool
+      deadPool.free(addEvent);    
+
     }
-    }
+  }
+}
     
 
 
